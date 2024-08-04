a32c123526548d5bb910e813b9a463a72785c2c8
AnAppleCore <994164148@qq.com>
2024-08-04 15:05:08 +0800

    160 baseline

--- run1.sh
+++ run1.sh
@@ -1,9 +1,11 @@
 CUDA_VISIBLE_DEVICES=6 \
 python train.py \
     --lr 0.001 \
-    --n_epoch 80 \
+    --n_epoch 160 \
     --batch_size 128 \
     --n_gen 3 \
     --dataset cifar100 \
-    --outdir ./workdirs/ban_cifar100_3gen_80epochs \
-    --resume_gen 1
+    --outdir ./workdirs/ban_cifar100_3gen_160epochs \
+    --resume_gen 0
+
+CUDA_VISIBLE_DEVICES=6 python infer.py --dataset cifar100 --weights_root ./workdirs/ban_cifar100_3gen_160epochs
